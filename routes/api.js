a2de48d357a1ae36191a2845b7ddb589d64dc9b4
Derek Wong <derekwdy@hotmail.com>
2022-09-21 03:09:35 -0400

    Initial working setup withboilerplate

---
+++ routes/api.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const Todo = require("../models/to-do-item");
+
+router.get("/to-dos", (req, res, next) => {
+  Todo.find({}, "action")
+    .then((data) => res.json(data))
+    .catch(next);
+});
+
+router.post("/to-dos", (req, res, next) => {
+  if (req.body.action) {
+    Todo.create(req.body)
+      .then((data) => res.json(data))
+      .catch(next);
+  } else {
+    res.json({
+      error: "The input field is empty",
+    });
+  }
+});
+
+router.delete("/to-dos/:id", (req, res, next) => {
+  Todo.findOneAndDelete({ _id: req.params.id })
+    .then((data) => res.json(data))
+    .catch(next);
+});
+
+module.exports = router;
